6d96f2fc9239b30d1fd37d5e549a77f53cd02e17
wslusarczyk <wioletkaslusarczyk@wp.pl>
2021-03-21 23:42:31 +0100

    Poprawki nazw

--- main.py
+++ main.py
@@ -13,30 +13,29 @@ choice = int(input("Wybierz co chcesz zrobić: \n 1)Dodać nową definicję, \n 
 if choice != 4:
     while(choice != 4):
         while (choice != 1 and choice != 2 and choice != 3 and choice != 4):
-            print("Wybór złej opcji, wybierz z pośród 1, 2, 3,4")
+            print("Wybór złej opcji, wybierz spośród 1, 2, 3, 4")
             choice = int(input(
-                "Wybierz co chcesz zrobić: \n 1)Dodać nową definicję, \n 2)Szukać istniejącej definicji, \n 3)Usunąć isniejącą definicje \n "))
+                "Wybierz co chcesz zrobić: \n 1)Dodać nową definicję, \n 2)Szukać istniejącej definicji, \n 3)Usunąć isniejącą definicje, \n 4)Zakończ program. \n "))
 
         if choice == 1:
             id = input("Podaj nazwę którą chcesz wprowadzić do słownika ")
-            title = input("Podaj definicję tej nazwy")
-            dictionary[id] = title
+            value = input("Podaj definicję tej nazwy")
+            dictionary[id] = value
             print("Definicja została dodana pomyślnie")
             while (choice != 1 and choice != 2 and choice != 3):
                 print("Wybór złej opcji, wybierz z pośród 1, 2, 3")
                 choice = int(input(
-                    "Wybierz co chcesz zrobić: \n 1)Dodać nową definicję, \n 2)Szukać istniejącej definicji, \n 3)Usunąć isniejącą definicje \n "))
+                    "Wybierz co chcesz zrobić: \n 1)Dodać nową definicję, \n 2)Szukać istniejącej definicji, \n 3)Usunąć isniejącą definicje, \n 4)Zakończ program. \n "))
         elif choice == 2:
-            title = input("Jakiej definicji szukasz ")
-            if title in dictionary:
-                print(title, "-", dictionary[title])
+            value = input("Jakiej definicji szukasz ")
+            if value in dictionary:
+                print(value, "-", dictionary[value])
             else:
                 print("Nie ma takiej definicji w słowniku")
         elif choice == 3:
-            lol = input("Jaką definicję chcesz usunąć ")
-            del dictionary[lol]
-            print("Definicja",lol, " została usunięta")
-        choice = int(input("Wybierz co chcesz zrobić: \n 1)Dodać nową definicję, \n 2)Szukać istniejącej definicji, \n 3)Usunąć isniejącą definicje \n "))
+            idToRemove = input("Jaką definicję chcesz usunąć ")
+            del dictionary[idToRemove]
+            print("Definicja", idToRemove, " została usunięta")
+        choice = int(input("Wybierz co chcesz zrobić: \n 1)Dodać nową definicję, \n 2)Szukać istniejącej definicji, \n 3)Usunąć isniejącą definicje, \n 4)Zakończ program. \n "))
 else:
     print("Zakończono")
-
